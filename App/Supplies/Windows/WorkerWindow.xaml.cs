238ef0e47e757de009b885def147cfa224da36bf
Ddatel35 <shumetov2004@gmail.com>
2024-04-09 19:56:01 +0300

    Added all files

---
+++ App/Supplies/Windows/WorkerWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Supplies.Frames;
+
+namespace Supplies.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkerWindow.xaml
+    /// </summary>
+    public partial class WorkerWindow : Window
+    {
+        public WorkerWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new OrdersPage());
+            Manager.MainFrame = MainFrame;
+        }
+    }
+}
