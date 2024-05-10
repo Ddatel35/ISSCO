37d6dcb99ef46644de955ac154de8874dd75848f
Ddatel35 <shumetov2004@gmail.com>
2024-05-11 02:32:18 +0300

    Added buttons on orders frame, create order window(no save order in DB)

--- App/Supplies/Frames/OrdersPage.xaml.cs
+++ App/Supplies/Frames/OrdersPage.xaml.cs
@@ -36,5 +36,56 @@ namespace Supplies.Frames
         {
             DGrid.ItemsSource = SuppliesDBEntities.GetContext().Orders.ToList();
         }
+
+        private void Add_Order(object sender, RoutedEventArgs e)
+        {
+            AddNewOrderWindow ANOW = new AddNewOrderWindow(null);
+            ANOW.ShowDialog();
+            UpdateTable();
+        }
+
+        private void Edit_Order(object sender, RoutedEventArgs e)
+        {
+            if (DGrid.SelectedItem != null)
+            {
+                AddNewOrderWindow ANOW = new AddNewOrderWindow(DGrid.SelectedItem as Orders);
+                ANOW.ShowDialog();
+            }
+            else
+                MessageBox.Show("Выберите заказ который хотите изменить!");
+
+            UpdateTable();
+        }
+
+        private void Del_Order(object sender, RoutedEventArgs e)
+        {
+            if (DGrid.SelectedItem != null)
+            {
+                var Removing = DGrid.SelectedItems.Cast<SuppliesT>().ToList();
+
+                if (MessageBox.Show($"Вы точно хотите удалить следующие {Removing.Count()} элеметнов?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        foreach (var rem in Removing)
+                        {
+                            SuppliesDBEntities.GetContext().Supplies.Remove(rem);
+                        }
+                        SuppliesDBEntities.GetContext().SaveChanges();
+                        MessageBox.Show("Данные удаленны");
+                        UpdateTable();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите удаляемый заказ!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
     }
 }
---
+++ App/Supplies/Windows/AddNewOrderWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Supplies.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Supplies.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewOrderWindow.xaml
+    /// </summary>
+    public partial class AddNewOrderWindow : Window
+    {
+        Orders _currentOrder = new Orders();
+
+        List<Components> selectedComponents = new List<Components>();
+
+        public AddNewOrderWindow(Orders selectedOrder)
+        {
+            InitializeComponent();
+
+            if (selectedOrder != null)
+            {
+                _currentOrder = selectedOrder;
+            }
+
+            fullNCB.ItemsSource = SuppliesDBEntities.GetContext().Clients.ToList();
+            compTypeCB.ItemsSource = SuppliesDBEntities.GetContext().Components_type.ToList();
+            compCB.ItemsSource = SuppliesDBEntities.GetContext().Components.ToList();
+
+            DataContext = _currentOrder;
+        }
+
+        private void Item_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            var tt = SuppliesDBEntities.GetContext().Components.ToList();
+
+            if (compTypeCB.SelectedIndex > 0)
+            {
+                tt = tt.Where(t => t.type_ID == (compTypeCB.SelectedItem as Components_type).ID).ToList();
+            }
+
+            compCB.ItemsSource = tt;
+        }
+
+        private void Selected_Item(object sender, SelectionChangedEventArgs e)
+        {
+            selectedComponents.Add(compCB.SelectedValue as Components);
+
+            Update_Table();
+        }
+
+        private void Update_Table()
+        {
+            DGrid.ItemsSource = selectedComponents.ToList();
+        }
+    }
+}
